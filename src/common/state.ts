fd8f5ffb0aa8e77e14420b83709bc5b09c083a98
Flower-Q <39539837+Flower-Q@users.noreply.github.com>
2020-04-18 19:55:51 -0300

    feat: 添加“置顶”菜单选项

--- src/common/state.ts
+++ src/common/state.ts
@@ -4,9 +4,13 @@ export type AppState = Instance<typeof AppState>;
 
 export const AppState = types
   .model({
+    alwaysOnTop: types.boolean,
     transparent: types.boolean
   })
   .actions(self => ({
+    setAlwaysOnTop: (value: boolean) => {
+      self.alwaysOnTop = value;
+    },
     setTransparent: (value: boolean) => {
       self.transparent = value;
     }
